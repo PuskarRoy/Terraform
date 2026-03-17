7ec369719349890164fbc10f9a8bac86221b23f3
Puskar Roy <roypuskar2003@gmail.com>
2026-03-17 15:20:21 +0530

    delete unnecessary files

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-# module "myvpc" {
-#   source       = "./module/vpc"
-#   aws_region   = var.aws_region
-#   project_name = var.project_name
-#   vpc_cidr     = var.vpc_cidr
-#   enable_ipv6  = var.enable_ipv6
-#   enable_nat   = var.enable_nat
-# }
