ad9f8e048e31764ec3c0711d616e32c950f69157
Puskar Roy <roypuskar2003@gmail.com>
2026-03-17 16:06:36 +0530

    Add Policy parameter in module/s3

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -28,5 +28,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = file(var.s3-policy-path)
+}
 
 
