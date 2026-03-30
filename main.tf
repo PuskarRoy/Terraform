cbb2fabf12280e0f37c10ea61629d1fec1ff1102
Puskar Roy <roypuskar2003@gmail.com>
2026-03-30 18:44:31 +0530

    add ec2 module

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-module "data-s3-bucket" {
-  source            = "./module/s3"
-  bucket_versioning = false
-  bucket_name       = "data-bucket"
-  encryption-type   = "aws:kms"
-  kms_key_id        = module.my-kms.arn
-}
-
-module "key_pair" {
+module "nat_key_pair" {
   source        = "./module/key-pair"
-  bucket_name   = module.data-s3-bucket.bucket_name
-  key_pair_name = "my-keypair"
+  bucket_name   = module.s3-bucket.bucket_name
+  key_pair_name = "nat-keypair"
 }
 
+module "s3-bucket" {
+  source            = "./module/s3"
+  bucket_name       = "test-bucket"
+  bucket_versioning = false
+
+}
 
 module "my-kms" {
   source          = "./module/kms"
@@ -19,24 +17,65 @@ module "my-kms" {
   kms-policy-path = "./assets/policies/kms-key-policy.json"
 }
 
-module "cloudtrail" {
-  source       = "./module/cloudtrail"
-  project_name = "my-project"
-}
-
 module "my-vpc" {
   source                = "./module/vpc"
-  enable_ipv6           = true
+  enable_ipv6           = false
   vpc_cidr              = "10.100.0.0/16"
   enable_nat            = true
   project_name          = "test"
   nat_ebs_kms           = module.my-kms.arn
   nat_ebs_volumn        = 10
-  nat_instance_key_pair = module.key_pair.key_pair_name
+  nat_instance_key_pair = module.nat_key_pair.key_pair_name
   nat_instance_type     = "t3.micro"
   nat_type              = "gateway"
   nat_instance_ami      = "ami-0f559c3642608c138"
   nat_instance_profile  = "ec2-admin"
+}
+
+
+module "ec2_key_pair" {
+  source        = "./module/key-pair"
+  bucket_name   = module.s3-bucket.bucket_name
+  key_pair_name = "private-ec2-keypair"
+}
+
+
+
+module "private-ec2" {
+  source                = "./module/ec2"
+  ami                   = "ami-0f559c3642608c138"
+  instance_profile      = "ec2-admin"
+  key_pair_name         = module.ec2_key_pair.key_pair_name
+  kms_key_id            = module.my-kms.arn
+  instance_type         = "t3.micro"
+  root_volumn_size      = 12
+  subnet_id             = module.my-vpc.private_subnets_ids[1]
+  vpc_security_group_id = aws_security_group.this.id
+
+}
+
+
+resource "aws_security_group" "this" {
+  name   = "Private-Ec2-SG"
+  vpc_id = module.my-vpc.vpc-id
+
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # For All Traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
 
--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,25 +1,57 @@
-# resource "aws_instance" "ec2" {
-#   ami = var.ami
-#   associate_public_ip_address = false
-#   instance_type = var.instance_type
-#   subnet_id = var.subnet_id
-#   disable_api_termination = true
-#   enable_primary_ipv6 = var.instance_ipv6
-#   iam_instance_profile = var.instance_profile
-#   force_destroy = true
-#   key_name = var.key_pair_name
-#   vpc_security_group_ids = [var.vpc_security_group_id]
-#   user_data = var.user_data
+resource "aws_instance" "ec2" {
+  ami                         = var.ami
+  associate_public_ip_address = false
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  disable_api_termination     = true
+  enable_primary_ipv6         = var.enable_instance_ipv6
+  iam_instance_profile        = var.instance_profile
+  force_destroy               = true
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = [var.vpc_security_group_id]
+  user_data                   = var.user_data_file_path
 
-#   root_block_device {
-#     delete_on_termination = false
-#     encrypted =  true
-#     volume_size = var.root_volumn_size
-#     volume_type = "gp3"
-#     kms_key_id = var.kms_key_id
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = true
+    volume_size           = var.root_volumn_size
+    volume_type           = "gp3"
+    kms_key_id            = var.kms_key_id
+  }
 
+  tags = var.tags
+
+}
+
+
+
+# resource "aws_security_group" "this" {
+#   name   = "EC2-SG"
+#   vpc_id = aws_vpc.main-vpc.id
+
+#   # lifecycle {
+#   #   create_before_destroy = true
+#   # }
+
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1" # For All Traffic
+#     cidr_blocks = [aws_vpc.main-vpc.cidr_block]
 #   }
 
-#   tags = var.tags
+#   egress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
 # }
